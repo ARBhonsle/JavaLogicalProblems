45a870de692b361538f4de5016030de692dc665c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-08 21:38:14 +0530

    Perfect Number calculated

---
+++ src/com/company/PerfectNumber.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    static void checkPerfectNumber(int number){
+        int sum=0,num=number;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==number){
+            System.out.println("Perfect Number");
+        }else{
+            System.out.println("Not Perfect Number");
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Perfect Number");
+        System.out.println("Give number: ");
+        Scanner sc=new Scanner(System.in);
+        int number= sc.nextInt();
+        checkPerfectNumber(number);
+    }
+}
