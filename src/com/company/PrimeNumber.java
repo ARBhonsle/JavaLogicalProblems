8252efe5e5495af203240c2625a7f6b0e9785ebe
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-08 21:51:15 +0530

    Prime Number calculated

---
+++ src/com/company/PrimeNumber.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    static void checkPrime(int number){
+        int count=0;
+        for(int i=2;i<number;i++){
+            if(number%i==0){
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("Prime Number");
+        }else{
+            System.out.println("Not Prime Number");
+        }
+
+    }
+    public static void main(String[] args){
+        System.out.println("Prime Number");
+        System.out.println("Give number: ");
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        checkPrime(number);
+    }
+}
