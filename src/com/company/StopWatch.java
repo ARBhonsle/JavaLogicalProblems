40381a1ab428d9431dc27cf394df280ae8f750ae
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-09 17:19:10 +0530

    Stop watch computed time elapsed

--- src/com/company/StopWatch.java
+++ src/com/company/StopWatch.java
@@ -1,2 +1,28 @@
-package com.company;public class StopWatch {
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * StopWatch shows time elapsed
+ */
+public class StopWatch {
+    static void task(int number){
+        for(int i=0;i<number;i++){
+            for(int j=i;j<number;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("StopWatch");
+        System.out.println("Give number:");
+        Scanner sc=new Scanner(System.in);
+        int number= sc.nextInt();
+        long begin=System.nanoTime();
+        task(number);
+        long end=System.nanoTime();
+        long timeElapsed=end-begin;
+        System.out.println("Time taken to print above: "+timeElapsed+" nano seconds");
+    }
 }
